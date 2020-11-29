11f0377904ec688d608e849b85e6d22c4ebd6810
chaulotj <chaulotj@gmail.com>
2020-11-29 16:24:54 -0500

    more AI stuff

--- Project-3/Project 3/Assets/Scripts/PlayerBehavior.cs
+++ Project-3/Project 3/Assets/Scripts/PlayerBehavior.cs
@@ -15,7 +15,7 @@ public class PlayerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!Pause.paused && Input.GetKey(KeyCode.LeftShift) && Vector3.Distance(monster.transform.position, transform.position) < runAlertDistance)
+        if (!Pause.paused && Input.GetKey(KeyCode.LeftShift) && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)) && Vector3.Distance(monster.transform.position, transform.position) < runAlertDistance)
         {
             monster.GetComponent<ArriveAtPoint>().NewPosition(transform.position);
         }
